0af76d92174da2f286dfcb2e637b56505d9cbc01
omer cakar <omerrccak@gmail.com>
2025-01-06 01:44:03 +0300

    starting codeing time logic

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes = ["soft":5,"medium":7,"hard":12]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        if let titleLabel = sender.titleLabel?.text {
+            let hardness = titleLabel.lowercased()
+            
+            switch hardness {
+            case "soft":
+                print("\(eggTimes[hardness]!)")
+            case "medium":
+                print("\(eggTimes[hardness]!)")
+            case "hard":
+                print("\(eggTimes[hardness]!)")
+            default:
+                print("Error")
+            }
+        }
+    }
+    
 }
 
