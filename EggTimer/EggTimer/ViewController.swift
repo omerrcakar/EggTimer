fae967d8898358377bfff4d6a4133f16c25f6c55
omer cakar <omerrccak@gmail.com>
2025-01-05 23:06:43 +0300

    Created Project

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by ÖMER  on 5.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
